66e8c68b5926fa31bb4da3d3d4b89b1053ae0456
Shaun <cq.xiang@foxmail.com>
2017-11-02 22:27:45 +0800

    Update upgrading.m

--- MLP/upgrading.m
+++ MLP/upgrading.m
@@ -1,12 +1,11 @@
 function net = upgrading(net)
+net.d_mw = net.moment * net.d_mw + net.lr * net.d_w;
+net.d_mv = net.moment * net.d_mv + net.lr * net.d_v;
+net.d_mwb = net.moment * net.d_mwb + net.lr * net.d_wb;
+net.d_mvb = net.moment * net.d_mvb + net.lr * net.d_vb;
 
-net.d_sw = net.moment * net.d_sw + net.lr*net.d_w
-net.d_sv = net.moment * net.d_sv + net.lr*net.d_v
-net.d_swb = net.moment * net.d_swb + net.lr*net.d_wb
-net.d_svb = net.moment * net.d_svb + net.lr*net.d_vb
-net.w = net.w - net.d_sw;
-net.v = net.v - net.d_sv;
-net.wb = net.wb - sum(net.d_swb,2)/net.batch_size;
-net.vb = net.vb - sum(net.d_svb,2)/net.batch_size;
-
+net.w = net.w - net.d_mw;
+net.v = net.v - net.d_mv;
+net.wb = net.wb - net.d_mwb;
+net.vb = net.vb - net.d_mvb;
 end
