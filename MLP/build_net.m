9a2b24ce597694f3c653818da6c26297efc657f5
Shaun <cq.xiang@foxmail.com>
2017-11-02 22:28:58 +0800

    Update build_net.m

--- MLP/build_net.m
+++ MLP/build_net.m
@@ -7,13 +7,24 @@ net.lr = learning_rate;
 net.batch_size = batch_size;
 net.moment = moment;
 
+%初始权值
 net.w = rand([net.num_hidden, net.num_input]) - 0.5;
 net.v = rand([net.num_output, net.num_hidden]) - 0.5;
+% net.w = normrnd(0,1,net.num_hidden, net.num_input);
+% net.v = normrnd(0,1,net.num_output, net.num_hidden);
 net.wb = zeros(net.num_hidden,1);
 net.vb = zeros(net.num_output,1);
 
-net.d_sw = zeros(size(net.w));
-net.d_sv = zeros(size(net.v));
-net.d_swb = zeros(size(net.wb));
-net.d_svb = zeros(size(net.vb));
+%初始梯度
+net.d_w = zeros(size(net.w));
+net.d_v = zeros(size(net.v));
+net.d_wb = zeros(size(net.wb));
+net.d_vb = zeros(size(net.vb));
+
+%初始迭代动量
+net.d_mw = zeros(size(net.w));
+net.d_mv = zeros(size(net.v));
+net.d_mwb = zeros(size(net.wb));
+net.d_mvb = zeros(size(net.vb));
+
 end
